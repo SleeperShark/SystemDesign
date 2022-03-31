a09b6d65faa2e06b339377997a1997c0c0cd415e
EC2 Default User <ec2-user@ip-172-31-9-253.ap-northeast-1.compute.internal>
2022-03-31 05:21:05 +0000

    add getShortUnique

--- server/route/shortUnique_route.js
+++ server/route/shortUnique_route.js
@@ -4,7 +4,7 @@ const { wrapAsync } = require('../../util/util');
 const { postShortUnique, getShortUnique } = require('../controller/shortUnique_controller');
 
 router.route('/shortUrlUnique').post(wrapAsync(postShortUnique));
-router.route('/shortUrlUnique/:shortUrl').post(wrapAsync(getShortUnique));
+router.route('/shortUrlUnique/:shortUrl').get(wrapAsync(getShortUnique));
 
 
 module.exports = router;
