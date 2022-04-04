90d93b7f233dd439acd7d54210f239a2f1277aa1
SleeperShark <88277367+SleeperShark@users.noreply.github.com>
2022-04-04 14:34:33 +0800

    Update k6.js

--- util/k6.js
+++ util/k6.js
@@ -1,22 +1,30 @@
-const http = require('k6/http');
-const { check, group, sleep } = require('k6');
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+
+const shortUrl = [
+    // short urls stored in database
+];
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export const options = {
     discardResponseBodies: true,
     scenarios: {
         contacts: {
             executor: 'constant-arrival-rate',
-            rate: 40,
+            rate: 250,
+            timeUnit: '1s',
             duration: '20s',
-            preAllocatedVUs: 5000,
-            // maxVUs: 100,
+            preAllocatedVUs: 10000,
         },
     },
 };
 
 const SLEEP_DURATION = 0.1;
 const body = JSON.stringify({
-    url: 'https://school.appworks.tw/',
+    url: 'https://www.google.com.tw/',
 });
 const params = {
     headers: {
@@ -24,11 +32,24 @@ const params = {
     },
 };
 
+// function for getting redirect from server
 export default function () {
     // Login request
-    const res = http.post('http://54.248.151.151/shortUrlByCache', body, params);
+    // const url = shortUrl[getRandom(0, shortUrl.length - 1)];
+    const res = http.post(http://systemdesign-1586369773.ap-northeast-1.elb.amazonaws.com/shortUrlUnique, body, params);
     check(res, {
         'status was 200': (r) => r.status == 200,
     });
     sleep(SLEEP_DURATION);
 }
+
+// function for establishing new short url 
+// export default function () {
+//     // Login request
+//     const url = shortUrl[getRandom(0, shortUrl.length - 1)];
+//     const res = http.get(http://systemdesign-1586369773.ap-northeast-1.elb.amazonaws.com/shortUrlUnique/${url});
+//     check(res, {
+//         'status was 200': (r) => r.status == 200,
+//     });
+//     sleep(SLEEP_DURATION);
+// }
